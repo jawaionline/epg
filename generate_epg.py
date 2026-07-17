2086369044af1be248fd5a927d63a3848e5b124c
jawaionline <yudi.kurniawan0306@gmail.com>
2026-07-17 22:59:45 +0700

    Update generate_epg.py

--- generate_epg.py
+++ generate_epg.py
@@ -24,8 +24,8 @@ os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 TITLE_REWRITE_RULES = {"NHL Hockey", "Live: NFL Football"}
 REMOTE_EPG_URLS = [
-    "https://github.com/BuddyChewChew/tcl-playlist-generator/raw/refs/heads/main/tcl_epg.xml",
-    "https://github.com/BuddyChewChew/xumo-playlist-generator/raw/refs/heads/main/playlists/xumo_epg.xml.gz",
+    "https://github.com/dbghelp/StarHub-TV-EPG/blob/main/starhub.xml",
+    "https://iptv-epg.org/files/epg-sg.xml",
     "https://github.com/matthuisman/i.mjh.nz/raw/refs/heads/master/PlutoTV/all.xml.gz",
     "https://github.com/matthuisman/i.mjh.nz/raw/refs/heads/master/Roku/all.xml.gz",
     "https://github.com/matthuisman/i.mjh.nz/raw/refs/heads/master/SamsungTVPlus/all.xml.gz",
